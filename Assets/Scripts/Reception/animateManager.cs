0e0e6987b389c7d4ce15a0e506545bf64c5c7dac
Agnijeet <agnijeetchoudhary@inucom.org>
2023-12-23 12:40:08 +0530

    nothing

--- Assets/Scripts/Reception/animateManager.cs
+++ Assets/Scripts/Reception/animateManager.cs
@@ -33,7 +33,7 @@ public class animateManager : MonoBehaviour
     public void setPosition()
     {
         collAnim = FindObjectOfType<sphereCollAnim>();
-        //pen.transform.parent = GameObject.FindGameObjectWithTag("Wrist").transform;
+        pen.transform.parent = GameObject.FindGameObjectWithTag("Wrist").transform;
         switch (collAnim.name)
         {
             case "model1_Sphere":
@@ -43,24 +43,25 @@ public class animateManager : MonoBehaviour
                 cards.transform.localPosition = new Vector3(0.306699991f, -0.105099998f, 0.0307f);
                 cards.transform.localEulerAngles = new Vector3(70.4637985f, 270.180664f, 276.008972f);
 
-/*                pen.transform.localPosition = new Vector3(-0.0379999988f, 0.0199999996f, 0.0527000017f);
-                pen.transform.localEulerAngles = new Vector3(342.850983f, 6.88359594f, 11.5986414);*/
+                pen.transform.localPosition = new Vector3(-0.124899998f, 0.0379999988f, -0.0313000008f);
+                pen.transform.localEulerAngles = new Vector3(348.443695f, 309.023071f, 179.78447f);
+
                 break;
 
             case "model2_Sphere":
                 cards.transform.localPosition = new Vector3(-0.0057f, -0.0229f, -0.036f);
                 cards.transform.localEulerAngles = new Vector3(8.626f, 260.123f, -106.012f);
 
-                pen.transform.localPosition = new Vector3(0.306699991f, -0.105099998f, 0.0307f);
-                pen.transform.localEulerAngles = new Vector3(70.4637985f, 270.180664f, 276.008972f);
+                pen.transform.localPosition = new Vector3(-0.0555999987f, 0.0935999975f, 0.0183000006f);
+                pen.transform.localEulerAngles = new Vector3(15.5909128f, 268.951782f, 14.6230659f);
                 break;
 
             case "model3_Sphere":
                 cards.transform.localPosition = new Vector3(-0.0057f, -0.0229f, -0.036f);
                 cards.transform.localEulerAngles = new Vector3(8.626f, 260.123f, -106.012f);
 
-                pen.transform.localPosition = new Vector3(0.306699991f, -0.105099998f, 0.0307f);
-                pen.transform.localEulerAngles = new Vector3(70.4637985f, 270.180664f, 276.008972f);
+                pen.transform.localPosition = new Vector3(-0.0555999987f, 0.0935999975f, 0.0183000006f);
+                pen.transform.localEulerAngles = new Vector3(15.5909128f, 268.951782f, 14.6230659f);
                 break;
         }
     }
@@ -94,14 +95,13 @@ public class animateManager : MonoBehaviour
     {
         setPosition();
         patient.GetComponent<Animator>().SetBool("Taken", true);
-        //pen.SetActive(true);
+        pen.SetActive(true);
     }
 
     public void giveBack()
     {
         setPosition();
         patient.GetComponent<Animator>().SetBool("GiveBack", true);
-        //pen.SetActive(false);
     }
 
     public void givenClipboard()
@@ -110,6 +110,7 @@ public class animateManager : MonoBehaviour
         patient.GetComponent<Animator>().SetBool("GivenClipboard", true);
         ID.GetComponent<ButtonManager>().isInteractable = true;
         ID.GetComponent<Button>().interactable = true;
+        pen.SetActive(false);
     }
 
     public void giveCards()
@@ -130,7 +131,7 @@ public class animateManager : MonoBehaviour
         patient.GetComponent<Animator>().SetBool("GivenCards", true);
         wait.GetComponent<ButtonManager>().isInteractable = true;
         wait.GetComponent<Button>().interactable = true;
-        if(currentMode.mode == Mode.Practice)
+        if (currentMode.mode == Mode.Practice)
         {
             scannerGhost.SetActive(true);
             scannerGhost.GetComponent<Animator>().Play("ID scanner");
